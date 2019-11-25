a2e9bee49dedfe7aaaad2b85fac136f0bf7f835e
Eduardo Aranda <eduardoarandah@gmail.com>
2019-11-25 12:13:22 -0600

    Agregar detalle de traslados

--- js/cargarxml.js
+++ js/cargarxml.js
@@ -1,46 +1,76 @@
 function cargarXML(theFile) {
-    return function(e) {
-        console.log('importando ' + theFile.name);
-        try {
-            //minusculas y reemplazar cfdi
-            var contenido = e.target.result.toUpperCase().replace(/<\?.+\?>/g, '').replace(/CFDI:/g, '').replace(/\n/g, '').replace(/\>\s+\</g, '\>\<');
-            // console.log(contenido);
-            // x=contenido;
-            //comprobante es el primer tag
-            var comprobante = $(contenido)[0];
-            emisor = $(comprobante).children("EMISOR")[0];
-            var out = {
-                archivo: theFile.name,
-                fecha: comprobante.attributes["FECHA"].value.replace(/T.+/g, ''),
-                rfc: emisor.attributes['RFC'].value,
-                nombre: emisor.attributes['NOMBRE'].value,
-                conceptos: [],
-                total: 0,
-                totalimpuestostrasladados: 0
-            };
-            if ($(comprobante).children("IMPUESTOS")[0]) {
-                out.totalimpuestostrasladados = parseFloat($(comprobante).children("IMPUESTOS")[0].attributes["TOTALIMPUESTOSTRASLADADOS"].value);
-            }
-            var conceptos = $(comprobante).children("CONCEPTOS").children("CONCEPTO");
-            conceptos.each(function(i) {
-                concepto = conceptos[i];
-                //obtener los atributos
-                var cantidad = parseInt($(concepto).attr('CANTIDAD'));
-                var descripcion = $(concepto).attr('DESCRIPCION');
-                var importe = parseFloat($(concepto).attr('IMPORTE'));
-                //asignar al objeto
-                out.conceptos.push({
-                    cantidad: cantidad,
-                    descripcion: descripcion,
-                    importe: importe
-                });
-            })            
-            app.agregar(out);
-        } catch (e) {
-            app.errores.push({
-                archivo: theFile.name,
-                mensaje: e.message
-            });
-        }
-    };
+  return function(e) {
+    console.log('importando ' + theFile.name);
+    try {
+      //minusculas y reemplazar cfdi
+      var contenido = e.target.result
+        .toUpperCase()
+        .replace(/<\?.+\?>/g, '')
+        .replace(/CFDI:/g, '')
+        .replace(/\n/g, '')
+        .replace(/\>\s+\</g, '><');
+      // console.log(contenido);
+      // x=contenido;
+      //comprobante es el primer tag
+      var comprobante = $(contenido)[0];
+      emisor = $(comprobante).children('EMISOR')[0];
+      var out = {
+        archivo: theFile.name,
+        fecha: comprobante.attributes['FECHA'].value.replace(/T.+/g, ''),
+        rfc: emisor.attributes['RFC'].value,
+        nombre: emisor.attributes['NOMBRE'].value,
+        conceptos: [],
+        total: 0,
+        totalimpuestostrasladados: 0,
+      };
+      if ($(comprobante).children('IMPUESTOS')[0]) {
+        out.totalimpuestostrasladados = parseFloat(
+          $(comprobante).children('IMPUESTOS')[0].attributes[
+            'TOTALIMPUESTOSTRASLADADOS'
+          ].value
+        );
+      }
+      var conceptos = $(comprobante)
+        .children('CONCEPTOS')
+        .children('CONCEPTO');
+      conceptos.each(function(i) {
+        concepto = conceptos[i];
+        //obtener los atributos
+        var cantidad = parseInt($(concepto).attr('CANTIDAD'));
+        var descripcion = $(concepto).attr('DESCRIPCION');
+        var importe = parseFloat($(concepto).attr('IMPORTE'));
+
+        // iteramos en los traslados
+        var traslados = [];
+        var impuestos = $(concepto)
+          .children('IMPUESTOS')
+          .children('TRASLADOS')
+          .children('TRASLADO');
+
+        impuestos.each(function(i) {
+          impuesto = impuestos[i];
+          var tasa = $(impuesto).attr('TASAOCUOTA');
+          var tipo = $(impuesto).attr('TIPOFACTOR');
+          traslados.push({
+            tasa: tasa,
+            tipo: tipo,
+          });
+        });
+
+        //asignar al objeto
+        out.conceptos.push({
+          cantidad: cantidad,
+          descripcion: descripcion,
+          importe: importe,
+          traslados: traslados,
+        });
+      });
+      app.agregar(out);
+    } catch (e) {
+      app.errores.push({
+        archivo: theFile.name,
+        mensaje: e.message,
+      });
+    }
+  };
 }
--- js/main.js
+++ js/main.js
@@ -1,64 +1,66 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        cfdis: [],
-        errores: []
+  el: '#app',
+  data: {
+    cfdis: [],
+    errores: [],
+  },
+  mounted: function() {
+    this.$refs.drop.addEventListener('dragover', this.handleDragOver, false);
+    this.$refs.drop.addEventListener('drop', this.handleFileSelect, false);
+  },
+  computed: {
+    total_suma: function() {
+      var total = 0;
+      for (var i = this.cfdis.length - 1; i >= 0; i--) {
+        total += this.total(this.cfdis[i]);
+      }
+      return total;
     },
-    mounted: function() {
-        this.$refs.drop.addEventListener('dragover', this.handleDragOver, false);
-        this.$refs.drop.addEventListener('drop', this.handleFileSelect, false);
+    total_impuestos_trasladados: function() {
+      var total = 0;
+      for (var i = this.cfdis.length - 1; i >= 0; i--) {
+        total += this.cfdis[i].totalimpuestostrasladados;
+      }
+      return total;
     },
-    computed: {
-        total_suma: function() {
-            var total = 0;
-            for (var i = this.cfdis.length - 1; i >= 0; i--) {
-                total += this.total(this.cfdis[i]);
-            }
-            return total;
-        },
-        total_impuestos_trasladados: function() {
-            var total = 0;
-            for (var i = this.cfdis.length - 1; i >= 0; i--) {
-                total += this.cfdis[i].totalimpuestostrasladados;
-            }
-            return total;
-        },
-        cantidad: function() {
-            return this.cfdis.length;
-        }
+    cantidad: function() {
+      return this.cfdis.length;
     },
-    methods: {
-        handleDragOver: function(evt) {
-            evt.stopPropagation();
-            evt.preventDefault();
-            evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-        },
-        handleFileSelect: function(evt) {
-            evt.stopPropagation();
-            evt.preventDefault();
-            var files = evt.dataTransfer.files; // FileList object.
-            for (var i = 0, f; f = files[i]; i++) {
-                var reader = new FileReader();
-                // Closure to capture the file information.
-                reader.onload = (cargarXML)(f);
-                // Read in the image file as a data URL.
-                reader.readAsText(f);
-            }            
-        },
-        total: function(item) {
-            var suma = 0;
-            for (var i = item.conceptos.length - 1; i >= 0; i--) {
-                suma += item.conceptos[i].importe;
-            }
-            return suma;
-        },
-        agregar:function(item){
-            //borrar si es que estuviera duplicado
-            var arr=this.cfdis.filter(function(el){
-                return el.archivo!=item.archivo;
-            });
-            arr.push(item);
-            this.cfdis=arr;
-        }
-    }
+  },
+  methods: {
+    handleDragOver: function(evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+    },
+    handleFileSelect: function(evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      var files = evt.dataTransfer.files; // FileList object.
+      for (var i = 0, f; (f = files[i]); i++) {
+        var reader = new FileReader();
+        // Closure to capture the file information.
+        reader.onload = cargarXML(f);
+        // Read in the image file as a data URL.
+        reader.readAsText(f);
+      }
+    },
+    total: function(item) {
+      var suma = 0;
+      for (var i = item.conceptos.length - 1; i >= 0; i--) {
+        suma += item.conceptos[i].importe;
+      }
+      return suma;
+    },
+    agregar: function(item) {
+      //borrar si es que estuviera duplicado
+      var arr = this.cfdis.filter(function(el) {
+        return el.archivo != item.archivo;
+      });
+      arr.push(item);
+      this.cfdis = arr;
+      console.log('cfdis:');
+      console.log(arr);
+    },
+  },
 });
